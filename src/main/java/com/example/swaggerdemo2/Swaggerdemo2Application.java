21079025b94b884e5268f423fbeaea3e163a7e87
Nobleson <noblesityav@gmail.com>
2024-08-16 11:45:42 +0100

    first commit

---
+++ src/main/java/com/example/swaggerdemo2/Swaggerdemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.swaggerdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swaggerdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swaggerdemo2Application.class, args);
+	}
+
+}
